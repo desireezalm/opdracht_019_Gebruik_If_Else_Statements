07b415f3e20d536a7127fd85e25be8d8c630c9b3
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2021-06-04 15:17:15 +0200

    Add files via upload

---
+++ ifelsestatements.js
@@ -0,0 +1,30 @@
+//DEEL 1
+const age = 18;
+
+if (age >= 18) {
+    console.log("U bent 18 jaar of ouder, dus u mag naar binnen")
+} else {
+    console.log("U bent jonger dan 18 jaar, dus U mag helaas niet naar binnen")
+}
+
+//na een constant een variable declareren werkt niet. Een constant blijft hetzelfde.
+
+
+//DEEL 2
+const isFemale = false;
+
+if (isFemale == true) {
+    console.log("U bent vrouw, dus u mag naar binnen")
+} else {
+    console.log("U bent geen vrouw, graag uw ID bewijs laten zien")
+}
+
+
+//DEEL 3
+const driverStatus = 'Bob';
+
+if (driverStatus == 'Bob') {
+    console.log("De bestuurder is de Bob, dus hij of zij mag rijden")
+} else {
+    console.log("De bestuurder is niet de Bob, dus hij of zij mag niet rijden")
+}
